385f98a23c61a06bf1af6de77a3f3be2cb6de23a
SH <email@email.com>
2017-10-25 19:18:39 -0500

    added cookie images. need formatting

--- app.py
+++ app.py
@@ -1,11 +1,14 @@
 # starting to determine routes
-from flask import Flask
+from flask import Flask,render_template
 
 app = Flask(__name__)
 
-@app.route('/')#route to home page
-def hello_world():
-    return 'hello world'
+@app.route('/')
+@app.route('/index.html')
+
+@app.route('/index', methods=['Get'])#route to home page
+def index():
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run() #kind of like the web server
