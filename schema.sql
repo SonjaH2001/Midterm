cd7578cab808b7fc1da2c1346f782ef394886a01
SH <email@email.com>
2017-10-27 12:04:37 -0500

    schema.sql creates tables

--- schema.sql
+++ schema.sql
@@ -1,19 +1,38 @@
 -- #database table
 
---create table(s)
 --find instruction or research the GUI (how to add table).
 -- #-->https://superuser.com/questions/461154/easy-simple-sqlite-table-database-gui
 -- #-->https://github.com/coleifer/sqlite-web
---1 design ERD (aka chicken foot) to get unstuck
 --maybe just get 2 tables to start (cookie type, customer,...)
 
 --straight up sql, the create table stuff
-
 --assuming sql file, and has all normalized tables
 -- import sqlite3 (not here!!)
 
--- sqlite_file = 'ClesiusCookies_db.sqlite'
--- table_cookie_type = 'Cookie_Type'
--- id_field = 'Cookie_ID'
-
 --bunch 0 inserts of test data TODO
+
+
+drop table if exists cookie_type;
+create table cookie_type (
+  cookei_id integer primary key autoincrement,
+  cookie_name text not null,
+  cookie_price text not null
+);
+
+drop table if exists customer;
+create table customer (
+  cust_id integer primary key autoincrement,
+  cust_name text not null,
+  cust_address text not null
+);
+
+drop table if exists orders;
+create table orders (
+  order_id integer primary key autoincrement,
+  cookie_id text not null,
+  cookie_
+  cust_id text not null
+);
+
+
+-- define constraints/FK PK, etc
