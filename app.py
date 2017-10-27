90c84626794ac7a2586432c228c1d4877a308538
SH <email@email.com>
2017-10-27 11:50:16 -0500

    routes to about and order page working. yay

--- app.py
+++ app.py
@@ -3,12 +3,21 @@ from flask import Flask,render_template
 
 app = Flask(__name__)
 
-@app.route('/')
-@app.route('/index.html')
-
-@app.route('/index', methods=['Get'])#route to home page
+# @app.route('/')
+# @app.route('/index.html')
+@app.route('/', methods=['Get'])#route to home page
 def index():
     return render_template('index.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/order')
+def order():
+    return render_template('order.html')
+
+
 
 if __name__ == '__main__':
     app.run(debug=True) #kind of like the web server
